eb19128e8dd4ab4e31adb9155a62ba07c03cb90a
Jon Gill <jon@recroom.com>
2022-10-27 18:02:40 -0700

    Add basic interfaces for IPromise and ICancelablePromise

---
+++ Packages/com.jonagill.promises/Runtime/ICancelablePromise.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace Promises
+{
+    public interface IReadOnlyCancelablePromise : IPromise
+    {
+        bool IsCanceled { get; }
+
+        ICancelablePromise Canceled(Action onCancel);
+        
+        ICancelablePromise ContinueWith(Func<ICancelablePromise> transformResult, Func<Exception, IPromise> transformException = null);
+        ICancelablePromise<T> ContinueWith<T>(Func<ICancelablePromise> transformResult, Func<Exception, IPromise<T>> transformException = null);
+        new ICancelablePromise<T> Transform<T>(Func<T> transformResult);
+        new ICancelablePromise TransformException(Func<Exception, Exception> transformResult);
+    }
+    
+    public interface ICancelablePromise : IReadOnlyCancelablePromise
+    {
+        CancellationToken CancellationToken { get; }
+        void Cancel();
+    }
+    
+    public interface IReadOnlyCancelablePromise<T> : IPromise<T>, IReadOnlyCancelablePromise
+    {
+        new ICancelablePromise<T> Canceled(Action onCancel);
+        
+        ICancelablePromise<U> ContinueWith<U>(Func<T, ICancelablePromise<U>> transformResult, Func<string, ICancelablePromise<U>> transformException = null);
+        ICancelablePromise ContinueWith(Func<T, ICancelablePromise> transformResult, Func<Exception, ICancelablePromise> transformException = null);
+        ICancelablePromise<U> Transform<U>(Func<T, U> transformResult);
+        ICancelablePromise<T> TransformException(Func<Exception, Exception> transformException);
+    }
+
+    public interface ICancelablePromise<T> : IPromise<T>, IReadOnlyCancelablePromise<T>, ICancelablePromise { }
+}
+
---
+++ Packages/com.jonagill.promises/Runtime/IPromise.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Promises 
+{
+    public interface IPromise
+    {
+        bool IsPending { get; }
+        bool HasSucceeded { get; }
+        bool HasException { get; }
+
+        IPromise Then(Action onComplete);
+        IPromise Catch(Exception onThrow);
+        IPromise Finally(Action onFinish);
+        
+        IPromise ContinueWith(Func<IPromise> transformResult, Func<Exception, IPromise> transformException = null);
+        IPromise<T> ContinueWith<T>(Func<IPromise<T>> transformResult, Func<Exception, IPromise<T>> transformException = null);
+        IPromise<T> Transform<T>(Func<T> transformResult);
+        IPromise TransformException(Func<Exception, Exception> transformResult);
+    }
+
+    public interface IPromise<T> : IPromise
+    {
+        IPromise<T> Then(Action<T> onComplete);
+        new IPromise<T> Then(Action onComplete);
+        new IPromise<T> Exception(Action<Exception> onThrow);
+        new IPromise<T> Finally(Action onFinish);
+        
+        IPromise<U> ContinueWith<U>(Func<T, IPromise<U>> transformResult, Func<string, IPromise<U>> transformException = null);
+        IPromise ContinueWith(Func<T, IPromise> transformResult, Func<Exception, IPromise> transformException = null);
+        IPromise<U> Transform<U>(Func<T, U> transformResult);
+        new IPromise<T> TransformException(Func<Exception, Exception> transformException);
+    }
+}
+
